3d73112b510f57431bc8e50579ec15894acc2f94
you <284908631@qq.com>
2020-06-13 12:22:39 +0800

    404，莫名解决了，好像是因为直接启动了servlet没有的get方法？添加了filter的init方法，错误点在adminFilter

--- src/filter/AdminFilter.java
+++ src/filter/AdminFilter.java
@@ -20,6 +20,10 @@ import javax.servlet.http.HttpSession;
  */
 public class AdminFilter implements Filter {
 
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+    }
+	
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
 		HttpServletRequest req = (HttpServletRequest) request;
 		HttpSession session = req.getSession();
